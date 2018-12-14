c1b4d82aeac956cec32a0f2b9b471c77a2edf801
Andrey Kartashov <porter@datirium.com>
2018-12-14 11:22:55 -0500

    RNA-Seq trim galore default analysis

--- biowardrobe_airflow_analysis/sql_patch/biowardrobe_tables/experimenttype_patch.sql
+++ biowardrobe_airflow_analysis/sql_patch/biowardrobe_tables/experimenttype_patch.sql
@@ -294,3 +294,122 @@ UPDATE `ems`.`experimenttype` SET
     "delete_files"
   ]'
 WHERE etype='DNA-Seq pair Trim Galore';
+
+
+-- RNA-Seq Trim Galore
+
+insert IGNORE into `ems`.`experimenttype` SELECT NULL, 'RNA-Seq Trim Galore', '', '', '';
+UPDATE `ems`.`experimenttype` SET
+  workflow='trim-rnaseq-se.cwl',
+  template='
+  {{
+    "fastq_file": {{"class": "File", "location": "{fastq_file_upstream}", "format": "http://edamontology.org/format_1930"}},
+    "star_indices_folder": {{"class": "Directory", "location": "{star_indices_folder}"}},
+    "bowtie_indices_folder": {{"class": "Directory", "location": "{bowtie_indices_folder_ribo}"}},
+    "chrom_length_file": {{"class": "File", "location": "{chrom_length}", "format": "http://edamontology.org/format_2330"}},
+    "annotation_file": {{"class": "File", "location": "{annotation_input_file}", "format": "http://edamontology.org/format_3475"}},
+    "exclude_chr": "{exclude_chr}",     "clip_3p_end": {clip_3p_end},     "clip_5p_end": {clip_5p_end},     "threads": {threads},
+    "output_folder": "{output_folder}",     "uid": "{uid}"
+  }}
+  ',
+  upload_rules='[
+    "upload_bigwig",
+    "upload_get_stat",
+    "upload_rpkm",
+    "upload_folder_size",
+    "delete_files"
+  ]'
+WHERE etype='RNA-Seq Trim Galore';
+
+-- RNA-Seq pair Trim Galore
+
+insert IGNORE into `ems`.`experimenttype` SELECT NULL, 'RNA-Seq pair Trim Galore', '', '', '';
+UPDATE `ems`.`experimenttype` SET
+  workflow='trim-rnaseq-pe.cwl',
+  template='
+    {{
+    "fastq_file_upstream": {{"class": "File", "location": "{fastq_file_upstream}", "format": "http://edamontology.org/format_1930"}},
+    "fastq_file_downstream": {{"class": "File", "location": "{fastq_file_downstream}", "format": "http://edamontology.org/format_1930"}},
+    "star_indices_folder": {{"class": "Directory", "location": "{star_indices_folder}"}},
+    "bowtie_indices_folder": {{"class": "Directory", "location": "{bowtie_indices_folder_ribo}"}},
+    "chrom_length_file": {{"class": "File", "location": "{chrom_length}", "format": "http://edamontology.org/format_2330"}},
+    "annotation_file": {{"class": "File", "location": "{annotation_input_file}",
+    "format": "http://edamontology.org/format_3475"}},
+    "exclude_chr": "{exclude_chr}",
+    "clip_3p_end": {clip_3p_end},
+    "clip_5p_end": {clip_5p_end},
+    "threads": {threads},
+    "output_folder": "{output_folder}",
+    "uid": "{uid}"
+    }}
+  ',
+  upload_rules='[
+    "upload_bigwig",
+    "upload_get_stat",
+    "upload_rpkm",
+    "upload_folder_size",
+    "delete_files"
+  ]'
+WHERE etype='RNA-Seq pair Trim Galore';
+
+-- RNA-Seq dUTP Trim Galore
+
+insert IGNORE into `ems`.`experimenttype` SELECT NULL, 'RNA-Seq dUTP Trim Galore', '', '', '';
+UPDATE `ems`.`experimenttype` SET
+  workflow='trim-rnaseq-se-dutp.cwl',
+  template='
+    {{
+      "fastq_file": {{"class": "File", "location": "{fastq_file_upstream}", "format": "http://edamontology.org/format_1930"}},
+      "star_indices_folder": {{"class": "Directory", "location": "{star_indices_folder}"}},
+      "bowtie_indices_folder": {{"class": "Directory", "location": "{bowtie_indices_folder_ribo}"}},
+      "chrom_length_file": {{"class": "File", "location": "{chrom_length}", "format": "http://edamontology.org/format_2330"}},
+      "annotation_file": {{"class": "File", "location": "{annotation_input_file}", "format": "http://edamontology.org/format_3475"}},
+      "exclude_chr": "{exclude_chr}",
+      "clip_3p_end": {clip_3p_end},
+      "clip_5p_end": {clip_5p_end},
+      "threads": {threads},
+      "output_folder": "{output_folder}",
+      "uid": "{uid}"
+    }}
+  ',
+  upload_rules='[
+    "upload_bigwig_upstream",
+    "upload_bigwig_downstream",
+    "upload_get_stat",
+    "upload_rpkm",
+    "upload_folder_size",
+    "delete_files"
+  ]'
+WHERE etype='RNA-Seq dUTP Trim Galore';
+
+-- RNA-Seq dUTP pair Trim Galore
+
+insert IGNORE into `ems`.`experimenttype` SELECT NULL, 'RNA-Seq dUTP pair Trim Galore', '', '', '';
+UPDATE `ems`.`experimenttype` SET
+  workflow='trim-rnaseq-pe-dutp.cwl',
+  template='
+  {{
+    "fastq_file_upstream": {{"class": "File", "location": "{fastq_file_upstream}", "format": "http://edamontology.org/format_1930"}},
+    "fastq_file_downstream": {{"class": "File", "location": "{fastq_file_downstream}", "format": "http://edamontology.org/format_1930"}},
+    "star_indices_folder": {{"class": "Directory", "location": "{star_indices_folder}"}},
+    "bowtie_indices_folder": {{"class": "Directory", "location": "{bowtie_indices_folder_ribo}"}},
+    "chrom_length_file": {{"class": "File", "location": "{chrom_length}", "format": "http://edamontology.org/format_2330"}},
+    "annotation_file": {{"class": "File", "location": "{annotation_input_file}", "format": "http://edamontology.org/format_3475"}},
+    "exclude_chr": "{exclude_chr}",
+    "clip_3p_end": {clip_3p_end},
+    "clip_5p_end": {clip_5p_end},
+    "threads": {threads},
+    "output_folder": "{output_folder}",
+    "uid": "{uid}"
+  }}
+  ',
+  upload_rules='[
+    "upload_bigwig_upstream",
+    "upload_bigwig_downstream",
+    "upload_get_stat",
+    "upload_rpkm",
+    "upload_folder_size",
+    "delete_files"
+  ]'
+WHERE etype='RNA-Seq dUTP pair Trim Galore';
+
